6b3417978c5cd5fcda37fffb041e34f25b3dd936
patrick-replogle <patrickr1138@gmail.com>
2020-07-31 18:45:39 -0700

    added a couple stretch built in intersection search methods in names.py

--- names/names.py
+++ names/names.py
@@ -37,3 +37,10 @@ print(f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# list comprehension performed search in 1.5 seconds
+duplicates = [name for name in names_1 if name in names_2]
+
+# Using sets finished search in 0.0229 seconds which was the fastest overall
+# & sign between two sets works the same as set(list1).intersection(list2)
+duplicates = set(names_1) & set(names_2)
