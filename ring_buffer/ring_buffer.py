0efdf500cf339370b487a17d2d92995dde93aee0
patrick-replogle <patrickr1138@gmail.com>
2020-07-31 18:20:51 -0700

    implemented ring buffer using list

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,28 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.oldest_index = None
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+            self.oldest_index = 0
+        elif len(self.storage) == self.capacity:
+            self.storage[self.oldest_index] = item
+            self.oldest_index += 1
+            if self.oldest_index == self.capacity:
+                self.oldest_index = 0
 
     def get(self):
-        pass
+        return self.storage
+
+
+r = RingBuffer(3)
+r.append('a')
+r.append('b')
+r.append('c')
+r.append('d')
+r.append('e')
+r.append('f')
+print(r.get())
