ba1b3ae4819d54c12099d535b1835d54b7ef8063
patrick-replogle <patrickr1138@gmail.com>
2020-07-31 19:04:38 -0700

    reimplemented the ring buffer using a doubly linked list

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,21 +1,55 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+# using a Doubly Linked List for storage
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = []
-        self.oldest_index = None
+        self.storage = DoublyLinkedList()
+        self.oldest_node = None
 
     def append(self, item):
-        if len(self.storage) < self.capacity:
-            self.storage.append(item)
-            self.oldest_index = 0
-        elif len(self.storage) == self.capacity:
-            self.storage[self.oldest_index] = item
-            self.oldest_index += 1
-            if self.oldest_index == self.capacity:
-                self.oldest_index = 0
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.oldest_node = self.storage.head
+        elif self.storage.length == self.capacity:
+            temp = self.oldest_node.get_next()
+            self.oldest_node.value = item
+            self.oldest_node = temp
+            if self.oldest_node is None:
+                self.oldest_node = self.storage.head
 
     def get(self):
-        return self.storage
+        storage_values = []
+        current = self.storage.head
+        count = 0
+
+        while count < self.storage.length:
+            storage_values.append(current.value)
+            current = current.get_next()
+            count += 1
+
+        return storage_values
+
+# using a list for storage
+# # class RingBuffer:
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.storage = []
+#         self.oldest_index = None
+
+#     def append(self, item):
+#         if len(self.storage) < self.capacity:
+#             self.storage.append(item)
+#             self.oldest_index = 0
+#         elif len(self.storage) == self.capacity:
+#             self.storage[self.oldest_index] = item
+#             self.oldest_index += 1
+#             if self.oldest_index == self.capacity:
+#                 self.oldest_index = 0
+
+#     def get(self):
+#         return self.storage
 
 
 r = RingBuffer(3)
